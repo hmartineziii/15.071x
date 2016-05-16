3ebedc045a527b1176a814f85486ec8043e82357
Hector Martinez <hmartineziii@gmail.com>
2016-05-15 21:34:59 -0700

    done with unit4

--- unit4/Unit4-HW-State.R
+++ unit4/Unit4-HW-State.R
@@ -14,6 +14,45 @@ LEpred2 = predict(linmod2)
 SSE2 = sum((statedata$Life.Exp - LEpred2)^2)
 SSE2
 
+#problem 2 build a CART model
+library(rpart)
+library(rpart.plot)
+CARTmodel = rpart(Life.Exp ~ ., data=statedata)
+prp(CARTmodel)
+PredictionsCART= predict(CARTmodel)
+tree.sse = sum((PredictionsCART - statedata$Life.Exp)^2)
+tree.sse
+CARTmodel2 = rpart(Life.Exp ~ ., data=statedata, minbucket=5)
+prp(CARTmodel2)
+PredictionsCART2 = predict(CARTmodel2)
+tree.sse2 = sum((PredictionsCART2 - statedata$Life.Exp)^2)
+tree.sse2
+CARTmodel3 = rpart(Life.Exp ~ Area, data=statedata, minbucket=1)
+prp(CARTmodel3)
+PredictionsCART3 = predict(CARTmodel3)
+tree.sse3 = sum((PredictionsCART3 - statedata$Life.Exp)^2)
+tree.sse3
 
+#problem3
+library(caret)
+set.seed(111)
+numFolds = trainControl(method="cv", number=10)
+cartGrid = expand.grid( .cp = seq(0.01,0.5,0.01))
+train(Life.Exp ~ ., data=statedata, method="rpart", trControl=numFolds, tuneGrid=cartGrid)
+CARTlifecp = rpart(Life.Exp ~ ., data=statedata, cp=0.11)
+prp(CARTlifecp)
+PredictionsCART4 = predict(CARTlifecp)
+tree.sse = sum((statedata$Life.Exp - PredictionsCART4)^2)
+tree.sse
+numFolds = trainControl(method="cv", number=10)
+cartGrid = expand.grid( .cp = seq(0.01,0.5,0.01))
+set.seed(111)
+train(Life.Exp ~ Area, data=statedata, method="rpart", trControl = numFolds, tuneGrid = cartGrid )
+set.seed(111)
+CARTmodel5 = rpart(Life.Exp ~ Area, data=statedata, cp=0.06)
+prp(CARTmodel5)
+PredictionsCART5 = predict(CARTmodel5)
+tree.sse = sum((statedata$Life.Exp - PredictionsCART5)^2)
+tree.sse
 
 
--- unit4/Unit4-HW-census.R
+++ unit4/Unit4-HW-census.R
@@ -1,19 +1,63 @@
 census = read.csv("census.csv")
 
 #problem1
+library(caTools)
+set.seed(2000)
+spl = sample.split(census$over50k, SplitRatio = 0.6)
+train = subset(census, spl == TRUE)
+test = subset(census, spl == FALSE)
+incomeLog = glm(over50k ~ ., data=train, family="binomial")
+summary(incomeLog)
+predictions = predict(incomeLog, newdata=test, type="response")
+table(test$over50k, predictions > 0.5)
+tab1 = table(test$over50k, predictions > 0.5)
+(tab1[1,1]+tab1[2,2])/nrow(test)
+table(test$over50k)
+9713/(9713+3078)
 
+#problem2
+library(rpart)
+library(rpart.plot)
+CARTo50k = rpart(over50k ~ ., data=train, method="class")
+prp(CARTo50k)
+predictions = predict(CARTo50k, newdata=test, type="class")
+table(test$over50k, predictions)
+(9243+1596)/nrow(test)
+library(ROCR)
+PredictROC = predict(CARTo50k, newdata = test)
+head(PredictROC)
+pred = prediction(PredictROC[,2], test$over50k)
+perf = performance(pred, "tpr", "fpr")
+plot(perf)
+as.numeric(performance(pred, "auc")@y.values)
+prp(CARTo50k)
 
+#problem3
+set.seed(1)
+trainSmall = train[sample(nrow(train), 2000), ]
+library(randomForest)
+set.seed(1)
+forestover50k = randomForest(over50k ~ ., data=trainSmall)
+predictForest = predict(forestover50k, newdata=test)
+table(test$over50k, predictForest)
+(9586+1093)/nrow(test)
+vu = varUsed(forestover50k, count=TRUE)
+vusorted = sort(vu, decreasing = FALSE, index.return = TRUE)
+dotchart(vusorted$x, names(forestover50k$forest$xlevels[vusorted$ix]))
+varImpPlot(forestover50k)
 
-
-
-
-
-
-
-
-
-
-
+#problem4 cross validation
+library(caret)
+library(e1071)
+set.seed(2)
+numFolds = trainControl(method="cv", number=10)
+cartGrid = expand.grid( .cp = seq(0.002,0.1,0.002))
+train(over50k ~ ., data=train, method="rpart", trControl=numFolds, tuneGrid=cartGrid)
+CART50kcp = rpart(over50k ~ ., data=train, method="class", cp=0.002)
+predictions = predict(CART50kcp, newdata=test, type="class")
+table(test$over50k, predictions)
+(9178+1838)/nrow(test)
+prp(CART50kcp)
 
 
 
